10af74fd04966e8ca055de0737106d2918872d66
adityarawat1337x <rkrawat98766@gmail.com>
2021-08-20 23:16:07 +0530

    built

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -5,3 +5,27 @@ export const addToCart = (item) => {
     payload: { ...item, quantity: 1 },
   };
 };
+export const increment = (id) => {
+  return {
+    type: "INCREMENT",
+    payload: id,
+  };
+};
+export const decrement = (id, quantity) => {
+  if (quantity === 1) {
+    return {
+      type: "REMOVE_FROM_CART",
+      payload: id,
+    };
+  }
+  return {
+    type: "DECREMENT",
+    payload: id,
+  };
+};
+export const removeFromCart = (id) => {
+  return {
+    type: "REMOVE_FROM_CART",
+    payload: id,
+  };
+};
